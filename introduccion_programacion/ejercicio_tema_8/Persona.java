de495471a6a3f7ecc04956913c5c9d0f9f682fc1
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-08-31 22:46:18 -0300

    ejercicio tema 8

---
+++ introduccion_programacion/ejercicio_tema_8/Persona.java
@@ -0,0 +1,36 @@
+public class Persona {
+  private Integer edad;
+  private String nombre;
+  private Integer telefono;
+
+  public Integer getEdad() {
+    return edad;
+  }
+  public void setEdad(Integer edad) {
+    this.edad = edad;
+  }
+  public String getNombre() {
+    return nombre;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+  public Integer getTelefono() {
+    return telefono;
+  }
+  public void setTelefono(Integer telefono) {
+    this.telefono = telefono;
+  }
+
+  public static void main(String[] args) {
+    Persona persona = new Persona();
+
+    persona.setEdad(22);
+    persona.setNombre("Ezequiel");
+    persona.setTelefono(1234567890);
+
+    System.out.println("edad: " + persona.getEdad());
+    System.out.println("nombre: " + persona.getNombre());
+    System.out.println("telefono: " + persona.getTelefono());
+  }
+}
