474769279cb76b0edc55cb9253e034fbb090fec9
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-08-31 22:45:53 -0300

    ejercicio tema 3

---
+++ introduccion_programacion/ejercicio_tema_3/Coche.java
@@ -0,0 +1,13 @@
+public class Coche {
+  public Integer puertas = 0;
+
+  public void incrementarPuerta() {
+    puertas += 1;
+  }
+
+  public static void main(String[] args) {
+    Coche miCoche = new Coche();
+    miCoche.incrementarPuerta();
+    System.out.println("cantidad de puertas: " + miCoche.puertas);
+  }
+}
---
+++ introduccion_programacion/ejercicio_tema_3/Sumar3Numeros.java
@@ -0,0 +1,9 @@
+public class Sumar3Numeros {
+  public static Integer suma(Integer numero1, Integer numero2, Integer numero3) {
+    return numero1 + numero2 + numero3;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("resultado de la suma: " + suma(1, 2, 3));
+  }
+}
